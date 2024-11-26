658397d675913aa5f505f8cdae5d4cf4285e542f
jasanch <jasanch@bancolombia.com.co>
2024-11-26 12:28:29 -0500

    add create cliente

--- controller/cliente.js
+++ controller/cliente.js
@@ -1,14 +1,33 @@
 const Cliente = require('../models/cliente');
 const { request, response } = require('express');
 
+const getClientes = async (req = request,
+    res = response) => {
+    try {
+        const clientesDB = await Cliente.find()//select * from tipoEquipo where estado=?
+        return res.json(clientesDB)
+    } catch (e) {
+        return res.status(500).json({
+            msg: 'Error general ' + e
+        })
+    }
+}
+
 const crearCliente = async (req = request, res = response) => {
     try {
         //obtener el nombre y email del cliente
         const nombre = req.body.nombre ? req.body.nombre.toUpperCase() : ''
         const email = req.body.email
 
+         // Validar que el nombre y el email estén presentes
+         if (!nombre || !email) {
+            return res.status(400).json({
+                msg: 'El nombre y el email son obligatorios',
+            });
+        }
+
         //verificar si ya existe un cliente con el mismo nombre
-        const clienteDB = await cliente.findOne({ nombre })
+        const clienteDB = await Cliente.findOne({ nombre })
 
         //si ya existe un cliente con el mismo nombre, retornar un error
         if (clienteDB) {
@@ -31,24 +50,15 @@ const crearCliente = async (req = request, res = response) => {
             cliente: clienteNuevo
         })
     } catch (e) {
+        console.log(e)
         return res.status(500).json({
-            msg: 'Error en el servidor' +
+            msg: 'Error en el servidor' + 
                 e
         })
     }
 }
 
-const getClientes = async (req = request,
-    res = response) => {
-    try {
-        const clientesDB = await Cliente.find()//select * from tipoEquipo where estado=?
-        return res.json(clientesDB)
-    } catch (e) {
-        return res.status(500).json({
-            msg: 'Error general ' + e
-        })
-    }
-}
+
 
 
 module.exports = {
