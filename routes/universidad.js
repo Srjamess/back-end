db757c0d8643ba28a6a7d0f944645858bd7bd40d
James Sanchez <srjamess63@gmail.com>
2025-07-27 12:38:47 -0500

    actializacion universidad'routes

--- routes/universidad.js
+++ routes/universidad.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
-const { getUniversidades, createUniversidad, deleteUniversidad } = require('../controller/universidad')
+const { getUniversidades, createUniversidad, deleteUniversidad, updateUniversidadById} = require('../controller/universidad')
 
 const router = Router()
 
 router.get('/', getUniversidades)
 router.post('/', createUniversidad)
-router.delete('/:id', deleteUniversidad)
 router.put('/:id', updateUniversidadById)
+router.delete('/:id', deleteUniversidad)
+
 
 
 module.exports = router
